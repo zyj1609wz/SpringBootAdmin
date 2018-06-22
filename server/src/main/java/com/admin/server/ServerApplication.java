6c2d591add6e966205b74d392688fb86e7ac4aca
zhaoyanjun <362299465@qq.com>
2018-06-22 17:34:35 +0800

    完成admin client 端配置

--- server/src/main/java/com/admin/server/ServerApplication.java
+++ server/src/main/java/com/admin/server/ServerApplication.java
@@ -2,9 +2,11 @@ package com.admin.server;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @EnableAdminServer
 public class ServerApplication {
 
