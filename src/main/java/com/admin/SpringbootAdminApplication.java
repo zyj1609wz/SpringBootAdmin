e9e49084b655cebf07cf9194ef62c2c883bbc230
zhaoyanjun <362299465@qq.com>
2018-06-22 16:31:48 +0800

    第一次提交

---
+++ src/main/java/com/admin/SpringbootAdminApplication.java
@@ -0,0 +1,12 @@
+package com.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAdminApplication.class, args);
+    }
+}
